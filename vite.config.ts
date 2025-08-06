bdd9af2335d485d34c9db773b8da8c0a3fc7d366
Dulina_Pathirana <n.dulina.pathirana.08@gmail.com>
2025-08-06 20:04:24 +1000

    Update source files

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/smartgigs7/',
+  base: '/',
   server: {
     host: "::",
     port: 8080,
